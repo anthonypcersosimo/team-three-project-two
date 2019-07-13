e957238704143b2b9483add17c36fb0b01ed5685
KDW <kyle.whittemore@gmail.com>
2019-07-13 12:28:03 -0400

    no hbrs

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 // Import routes and give the server access to them.
 var routes = require("./controllers/flashcards_controller.js");
 
