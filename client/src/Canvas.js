4eac93e64fc9aec22e419b5ba49e41ad93719f88
KDW <kyle.whittemore@gmail.com>
2019-07-12 22:18:49 -0400

    basic code styled deckCard component

--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DeckCard from './components/deckCard'
 
 const Canvas = () => {
+
+    const testDeck = {
+        title: "React Basics",
+        description: "A short introduction to the basics of React: state, props, hooks, etc."
+    }
+
+    const [deck] = useState(testDeck)
+
     return (
         <div>
             <div>
                 <h1>Style That Snizz!</h1>
             </div>
             <div className="m-4">
-                <DeckCard />
+                <DeckCard deck={deck}/>
             </div>
         </div>
     )
--- client/src/components/deckCard.js
+++ client/src/components/deckCard.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Canvas.css';
 import '../index.css';
 
-const DeckCard = () => {
+const DeckCard = (props) => {
+    
+    const [deck] = useState(props.deck)
+
     return (
-        <div className="card col-md-3 code-font">
+        <div className="card deck-card code-font shadow p-3 mb-5 bg-white rounded">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title font-maroon">{deck.title}</h5>
+                <br></br>
+                <p className="card-text">{deck.description}</p>
             </div>
         </div>
     )
