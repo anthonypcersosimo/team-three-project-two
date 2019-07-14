99a7888153e7cfd64013b703a3c91950507edbd2
KDW <kyle.whittemore@gmail.com>
2019-07-14 12:11:38 -0400

    tested all routes with postman, all work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
     //---------------------------------------------------------//
 
     // GET route for getting all of the flashcards
+    // Tested working
     app.get("/api/flashcards/", function (req, res) {
         db.Flashcard.findAll({})
             .then(function (dbFlashcard) {
@@ -15,6 +16,7 @@ module.exports = function (app) {
     });
 
     // POST route for saving a new flashcard
+    // Tested working
     app.post("/api/flashcards", function (req, res) {
         console.log(req.body);
         db.Flashcard.create({
@@ -29,7 +31,8 @@ module.exports = function (app) {
     });
 
     // Get route for returning all cards for a specific deck
-    app.get("/api/decks/:id", function (req, res) {
+    // Tested working
+    app.get("/api/flashcards/deck/:id", function (req, res) {
         db.Flashcard.findAll({
             where: {
                 deckId: req.params.id
@@ -41,6 +44,7 @@ module.exports = function (app) {
     });
 
     // PUT route for updating cards
+    // Tested working
     app.put("/api/flashcards", function (req, res) {
         db.Flashcard.update(
             req.body,
@@ -54,6 +58,7 @@ module.exports = function (app) {
     });
 
     // DELETE route for deleting cards
+    // Tested working
     app.delete("/api/flashcards/:id", function (req, res) {
         db.Flashcard.destroy({
             where: {
@@ -69,6 +74,7 @@ module.exports = function (app) {
     //---------------------------------------------------------//
 
     // GET route for getting all of the decks
+    // Tested working
     app.get("/api/decks", function (req, res) {
         db.Deck.findAll({})
             .then(function (dbDeck) {
@@ -77,6 +83,7 @@ module.exports = function (app) {
     });
 
     // POST route for saving a new deck
+    // Tested working
     app.post("/api/decks", function (req, res) {
         // console.log(req.body);
         db.Deck.create({
@@ -88,16 +95,16 @@ module.exports = function (app) {
     });
 
     // DELETE route for deleting decks
+    //  Tested working
     app.delete("/api/decks/:id", function (req, res) {
-        db.Flashcard.destroy({
+        db.Deck.destroy({
             where: {
                 id: req.params.id
             }
-        }).then(function (dbFlashcard) {
-            res.json(dbFlashcard);
+        }).then(function (dbDeck) {
+            res.json(dbDeck);
         });
     });
-
 
 };
 
