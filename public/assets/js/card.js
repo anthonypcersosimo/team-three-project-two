6a87426e83af837f8f4dfa0693b0eecb2a042217
Jimbomoso <jimbomoso@gmail.com>
2019-07-15 09:55:04 -0400

    Added todos to cards js

--- public/assets/js/card.js
+++ public/assets/js/card.js
@@ -1,3 +1,15 @@
+// changes the class of the card face which animates the flip and toggles the side shown
 function flip() {
     $('.flip-card-inner').toggleClass('flipped');
 }
+
+// need way to get deck name and display in deck title
+
+// need way to get card from selected deck and display question on question side of card/
+
+// need way to get card from selected deck and display answer on answer side of card
+
+// need way to get last card
+
+// need way to get next card
+
