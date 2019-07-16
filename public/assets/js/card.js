5c1b6283abae389ef724ba2f23e9249908bf50ec
Jimbomoso <jimbomoso@gmail.com>
2019-07-16 18:50:56 -0400

    added: onload function getCards url searcher deckID getter

--- public/assets/js/card.js
+++ public/assets/js/card.js
@@ -3,28 +3,36 @@ $(document).ready(function() {
     let deckName;
     let deck = []; 
     
+    // assigns the url from the lead in page 
+    let url = window.location.search;
+    
     const getCards = deckId => $.get("api/flashcards/deck/" + deckId, response => {
         deck = response;
-        console.log(deck)
-    })
+        console.log("deck response", deck)
+        renderTable(deck)
+    });
+    // grabs the deck id from the lead in page's url and assigns it to deckID
+    if (url.indexOf("?deck_id=") !== -1) {
+        deckId = url.split("=")[1];
+        console.log(deckId)
+        $("#deck-form").addClass("hidden");
+        getCards(deckId);
+    };
+
+    function roll() {
+        $('.roll-header-inner').toggleClass('rolled');
+        setTimeout(function(){ flip(); }, 0500);
+    };
+
+    // changes the class of the card face which animates the flip and toggles the side shown
+    function flip() {
+        $('.flip-card-inner').toggleClass('flipped');
+    };
+
 });
-
-function roll() {
-    $('.roll-header-inner').toggleClass('rolled');
-    setTimeout(function(){ flip(); }, 0500);
-};
-
-// changes the class of the card face which animates the flip and toggles the side shown
-function flip() {
-    $('.flip-card-inner').toggleClass('flipped');
-};
-
 
 // Questions I have:
 
 // #1) When a user selects a deck at the previous screen, what tells my files what was selected so they know what to load?
 
 // #2) How do I take the selected deck and split into individual pairings of questions/answers (single card) and what becomes the card identifer? (for going back and forth through the deck)
-
-
-
