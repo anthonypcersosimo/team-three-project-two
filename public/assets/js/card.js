a7744e7ca593a61c6fb40f1252794fee9a91ca59
KDW <kyle.whittemore@gmail.com>
2019-07-15 21:39:49 -0400

    .

--- public/assets/js/card.js
+++ public/assets/js/card.js
@@ -1,27 +1,3 @@
-$(document).ready(function () {
-    
-    
-    
-    
-    let currentDeck = [];
-
-    $.get("/api/flashcards/deck/1", function (result) {
-        currentDeck = result;
-        console.log(currentDeck)
-    });
-    // changes the class of the card face which animates the flip and toggles the side shown
-    function flip() {
-        $('.flip-card-inner').toggleClass('flipped');
-    };
-
-});
-
-// need way to get deck name and display in deck title
-
-// need way to get card from selected deck and display question on question side of card/
-
-// need way to get card from selected deck and display answer on answer side of card
-
-// need way to get last card
-
-// need way to get next card
+function flip() {
+    $('.flip-card-inner').toggleClass('flipped');
+}
