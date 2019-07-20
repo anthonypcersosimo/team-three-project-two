8d6744e138549fd5686c65244ab2a2e99a3fe051
KDW <kyle.whittemore@gmail.com>
2019-07-19 23:02:57 -0400

    formatting

--- routes/pdfExport.js
+++ routes/pdfExport.js
@@ -2,40 +2,34 @@ const PDFdoc = require('pdfkit');
 const fs = require('fs');
 var db = require("../models");
 
-// const doc = new PDFdoc;
-
-// doc.pipe(fs.createWriteStream('./output.pdf'));
-
 module.exports = function (app) {
 
     const doc = new PDFdoc;
 
     doc.pipe(fs.createWriteStream('./output.pdf'));
 
-    // doc.text('foo bar!', 100, 100)
-    // doc.end();
-
-
-    app.get("/api/flashcards/pdf", function (req, res) {
+    app.get("/api/flashcards/deck/pdf/:id", function (req, res) {
         db.Flashcard.findAll({
-            where: {},
+            where: {
+                deckId: req.params.id
+            },
             include: [db.Deck]
         })
-            .then(function (dbFlashcard) {
-                
-                dbFlashcard.forEach(card => {
+            .then(function (dbDeck) {
+
+                dbDeck.forEach(card => {
                     doc.text(card.term, {
                         // lineBreak: false
                     })
                     doc.moveDown(2);
-                    doc.text(card.def,{
+                    doc.text(card.def, {
                         // lineBreak: false
                     })
                     doc.moveDown(2);
                 })
-                
+
                 res.json(dbFlashcard);
-            }).then(function() {
+            }).then(function () {
 
                 doc.end();
             });
